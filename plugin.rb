bacb30f918826e54b0b55e8330abb671a73229f4
Jeff Wong <awole20@gmail.com>
2018-06-17 14:20:41 -0700

    init

---
+++ plugin.rb
@@ -0,0 +1,44 @@
+# name: discourse-hibp-password-validator
+# about: A password validator using Troy Hunt's Have I Been Pwned API
+# version: 0.0.1
+# authors: Jeff Wong
+
+require 'net/http'
+require 'digest/sha1'
+
+enabled_site_setting :hibp_validation_enabled
+
+after_initialize do
+  module ::DiscourseHibp
+
+    class HibpPasswordValidator < ActiveModel::EachValidator
+
+      def validate_each(record, attribute, value)
+        return unless record.password_validation_required?
+        if pwned_password(value)
+          record.errors.add(attribute, :hibp_common)
+        end
+      end
+
+      def pwned_password(value)
+        ::Rails.logger.info "pwned password??"
+
+        hash = Digest::SHA1.hexdigest(value)
+        ::Rails.logger.info hash
+        hash_start = hash.slice(0,5)
+        ::Rails.logger.info hash_start
+        uri = URI("https://api.pwnedpasswords.com/range/#{hash_start}")
+        result = Net::HTTP.get(uri)
+        ::Rails.logger.info result
+        return false
+      end
+    end
+  end
+
+  class User
+    validate :hibp_password_validator
+    def hibp_password_validator
+      DiscourseHibp::HibpPasswordValidator.new(attributes: :password).validate_each(self, :password, @raw_password)
+    end
+  end
+end
